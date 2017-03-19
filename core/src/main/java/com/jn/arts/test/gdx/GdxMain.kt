a4d1736de056e637c618ae6c50a3346a5327143e
devjn <Emperor117@rambler.ru>
2017-03-19 23:11:28 +0200

    Updated Kotlin code null safety

--- core/src/main/java/com/jn/arts/test/gdx/GdxMain.kt
+++ core/src/main/java/com/jn/arts/test/gdx/GdxMain.kt
@@ -8,16 +8,15 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
 /** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms.  */
 class GdxMain() : ApplicationAdapter() {
-    private var batch: SpriteBatch? = null ;///= SpriteBatch()
-    private var image: Texture? = null
-    private var etc1Image: Texture? = null;
-    private var etc2Image: Texture? = null;
-    var testing = false;
+    private lateinit var batch: SpriteBatch
+    private lateinit var image: Texture
+    private var etc1Image: Texture? = null
+    private var etc2Image: Texture? = null
+    var testing = false
 
     constructor(testing: Boolean) : this() {
         this.testing = testing
     } // either super() or delegate to another constructor
-
 
 
     override fun create() {
@@ -30,18 +29,18 @@ class GdxMain() : ApplicationAdapter() {
     }
 
     override fun render() {
-        if(testing) return;
+        if(testing) return
         Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch!!.begin()
-        batch!!.draw(image!!, 165f, 180f)
-        batch!!.draw(etc1Image!!, 50f, 300f)
-        batch!!.draw(etc2Image!!, 350f, 300f)
-        batch!!.end()
+        batch.begin()
+        batch.draw(image, 165f, 180f)
+        etc1Image?: batch.draw(etc1Image, 50f, 300f)
+        etc2Image?: batch.draw(etc2Image, 350f, 300f)
+        batch.end()
     }
 
     override fun dispose() {
-        batch!!.dispose()
-        image!!.dispose()
+        batch.dispose()
+        image.dispose()
     }
 }
